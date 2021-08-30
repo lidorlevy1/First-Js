7f0f83faebd4e72631037518d359f46b998dc580
lidorlevy1 <89572109+lidorlevy1@users.noreply.github.com>
2021-08-30 23:01:52 +0300

    EXE4

--- Exercises2.js
+++ Exercises2.js
@@ -24,3 +24,4 @@ console.log(solutionB,solutionA);
 }
 
 // function
+
---
+++ Exercises4.js
@@ -0,0 +1,25 @@
+// Here's a template you can use:
+const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
+// your code goes here
+function range(arr){
+let finalerange= Math.max(...arr)-Math.min(...arr)
+ return finalerange
+}
+console.log(range([73, 91, 88, 56, 62, 84, 95, 96, 88, 92]))
+
+
+function median(arr1){
+let medi=arr1.sort()
+ let finaelMedian=medi.length()
+ return finaelMedian
+
+ 
+}
+
+
+function halfrange(arr3){
+ let Halfrange=arr3.slice(5,10);
+ let finaelhalfrange=Math.max(...Halfrange)-Math.min(...Halfrange);
+ return finaelhalfrange
+}
+console.log(halfrange([73, 91, 88, 56, 62, 84, 95, 96, 88, 92]))
