b3a6c4b6a1d98d2ed1ce2d0187b1cc40d9c31e7f
lidorlevy1 <89572109+lidorlevy1@users.noreply.github.com>
2021-09-02 15:05:40 +0300

    Update Formula 1 races.js

--- Formula 1/Formula 1 races.js
+++ Formula 1/Formula 1 races.js
@@ -1,36 +1,36 @@
-// list of Formula 1 champions since the year 2000
-const formula1Champions = [
- "Schumacher",
- "Schumacher",
- "Schumacher",
- "Schumacher",
- "Schumacher",
- "Alonso",
- "Alonso",
- "Räikkönen",
- "Hamilton",
- "Button",
- "Vettel",
- "Vettel",
- "Vettel",
- "Vettel",
- "Hamilton",
- "Hamilton",
- "Rosberg",
- "Hamilton",
- "Hamilton",
- "Hamilton",
- "Hamilton"
-]
-let winners ={} /*{Hamilton:"7wins", Schumacher:"5 wins" ,Vettel:"4 wins", Alonso:"2 wins",Räikkönen:"1 win", Button:"1 win", Rosberg:"1 win" }*/
+// // list of Formula 1 champions since the year 2000
+// const formula1Champions = [
+//  "Schumacher",
+//  "Schumacher",
+//  "Schumacher",
+//  "Schumacher",
+//  "Schumacher",
+//  "Alonso",
+//  "Alonso",
+//  "Räikkönen",
+//  "Hamilton",
+//  "Button",
+//  "Vettel",
+//  "Vettel",
+//  "Vettel",
+//  "Vettel",
+//  "Hamilton",
+//  "Hamilton",
+//  "Rosberg",
+//  "Hamilton",
+//  "Hamilton",
+//  "Hamilton",
+//  "Hamilton"
+// ]
+// let winners ={} /*{Hamilton:"7wins", Schumacher:"5 wins" ,Vettel:"4 wins", Alonso:"2 wins",Räikkönen:"1 win", Button:"1 win", Rosberg:"1 win" }*/
 
-function countWin(driver) {
- // your code goes here
- if(winners.hasOwnProperty(driver)){
-   winners[driver] 
- }   else {
-  winners[driver] = 1
-}
-}
+// function countWin(driver) {
+//  // your code goes here
+//  if(winners.hasOwnProperty(driver)){
+//    winners[driver] 
+//  }   else {
+//   winners[driver] = 1
+// }
+// }
 
-formula1Champions.forEach(countWin)
+// formula1Champions.forEach(countWin)
